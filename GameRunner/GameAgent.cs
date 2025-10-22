b90bab8848cf6d8f4fa595da53496d91e4c3f00e
yusuken64 <61254107+yusuken64@users.noreply.github.com>
2025-10-22 03:33:07 -0400

    add agent

---
+++ GameRunner/GameAgent.cs
@@ -0,0 +1,127 @@
+﻿using CardBattleEngine;
+
+public interface IGameAgent
+{
+	public IGameAction GetNextAction(GameState game);
+}
+
+public class AttackFaceAI : IGameAgent
+{
+	private readonly Player _player;
+
+	public AttackFaceAI(Player player)
+	{
+		_player = player;
+	}
+
+	public IGameAction GetNextAction(GameState game)
+	{
+		var validActions = game.GetValidActions(_player);
+		return validActions[0];
+	}
+}
+
+public class AttackMinionAI : IGameAgent
+{
+	private readonly Player _player;
+
+	public AttackMinionAI(Player player)
+	{
+		_player = player;
+	}
+
+	public IGameAction GetNextAction(GameState game)
+	{
+		var validActions = game.GetValidActions(_player);
+		return validActions.Skip(1).FirstOrDefault();
+	}
+}
+
+public class RandomAI : IGameAgent
+{
+	private readonly Player _player;
+	private readonly IRNG _rng;
+
+	public RandomAI(Player player, IRNG rng)
+	{
+		_player = player;
+		_rng = rng;
+	}
+
+	public IGameAction GetNextAction(GameState game)
+	{
+		var validActions = game.GetValidActions(_player);
+
+		return ChooseRandom(validActions);
+	}
+	public T ChooseRandom<T>(IReadOnlyList<T> options)
+	{
+		if (options.Count == 0) return default!;
+		return options[_rng.NextInt(0, options.Count)];
+	}
+}
+
+public class HumanAgent : IGameAgent
+{
+	private readonly Player _player;
+
+	public HumanAgent(Player player)
+	{
+		_player = player;
+	}
+
+	public IGameAction GetNextAction(GameState state)
+	{
+		var actions = state.GetValidActions(_player).ToList();
+		int selectedIndex = 0;
+
+		
+		// Reserve bottom lines of the console for menu
+		int optionStartTop = Math.Max(0, Console.WindowTop + Console.WindowHeight - actions.Count - 1);
+
+		for (int i = 0; i < actions.Count; i++)
+		{
+			Console.WriteLine();
+		}
+		while (true)
+		{
+			// Draw each option in place
+			for (int i = 0; i < actions.Count; i++)
+			{
+				int line = optionStartTop + i;
+				if (line >= Console.BufferHeight) line = Console.BufferHeight - 1;
+
+				Console.SetCursorPosition(0, line);
+
+				// Clear the line first
+				Console.Write(new string(' ', Console.BufferWidth));
+				Console.SetCursorPosition(0, line);
+
+				// Draw the option
+				if (i == selectedIndex)
+				{
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.Write($"> {actions[i]}");
+					Console.ResetColor();
+				}
+				else
+				{
+					Console.Write($"  {actions[i]}");
+				}
+			}
+
+			// Read input
+			var key = Console.ReadKey(true);
+			if (key.Key == ConsoleKey.UpArrow)
+				selectedIndex = (selectedIndex - 1 + actions.Count) % actions.Count;
+			else if (key.Key == ConsoleKey.DownArrow)
+				selectedIndex = (selectedIndex + 1) % actions.Count;
+			else if (key.Key == ConsoleKey.Enter)
+				break;
+		}
+
+		// Move cursor below menu
+		Console.SetCursorPosition(0, optionStartTop + actions.Count);
+		return actions[selectedIndex];
+	}
+}
--- GameRunner/Program.cs
+++ GameRunner/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CardBattleEngine;
+
+var gameState = GameFactory.CreateTestGame();
+var engine = new GameEngine(new XorShiftRNG(1));
+
+//IGameAI player1 = new RandomAI(gameState.Players[0], new XorShiftRNG(1));
+IGameAgent player1 = new HumanAgent(gameState.Players[0]);
+IGameAgent player2 = new RandomAI(gameState.Players[1], new XorShiftRNG(1));
+
+bool stopPerAction = false;
+
+engine.StartGame(gameState);
+while (!gameState.IsGameOver())
+{
+	var currentAgent = gameState.CurrentPlayer == gameState.Players[0] ? player1 : player2;
+	var action = currentAgent.GetNextAction(gameState);
+	engine.Resolve(gameState, gameState.CurrentPlayer, gameState.OpponentPlayer, action);
+
+	if (stopPerAction)
+	{
+		Console.ReadLine();
+	}
+}
+
+Console.WriteLine($"Winner: {gameState.Winner?.Name}");
