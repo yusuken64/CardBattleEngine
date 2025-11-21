758e59fadfb61e82be7148b29d211924af15bfc9
yusuken64 <61254107+yusuken64@users.noreply.github.com>
2025-11-21 02:18:42 -0500

    Update HeroPower.cs

--- CardBattleEngine/Entities/HeroPower.cs
+++ CardBattleEngine/Entities/HeroPower.cs
@@ -2,6 +2,7 @@
 
 public class HeroPower
 {
+	public string Name { get; set; }
 	public int ManaCost { get; set; }
 	public bool UsedThisTurn { get; set; } = false;
 	public TargetingType TargetingType { get; set; }
